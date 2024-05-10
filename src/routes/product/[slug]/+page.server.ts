3db94febe6e3d8d43e21603931a593af4485aa3f
sakib3046 <sakib3046@gmail.com>
2024-05-10 18:24:21 +0600

    product page image, title rendering by slug (from url)

--- src/routes/product/[slug]/+page.server.ts
+++ src/routes/product/[slug]/+page.server.ts
@@ -2,9 +2,13 @@ import { sanityClient } from "$lib/sanityClient";
 import { error } from '@sveltejs/kit';
 
 
-export async function load() {
-    const productQuery ='*[_type == "product" && slug.current ==]';
+export async function load({ params }) {
+    const productSlug = params.slug
+    const productQuery =`*[_type == "product" && slug.current == '${productSlug}'][0]`;
     const productData = await sanityClient.fetch(productQuery);
+
+    
+    console.log(productData)
 
     if (!{productData}) {
         throw error(404);
--- src/routes/product/[slug]/+page.svelte
+++ src/routes/product/[slug]/+page.svelte
@@ -1,7 +1,16 @@
-<script>
-    // import { page } from "$app/stores";
-    import { productSlug } from "./store";
-    // const productSlug = $page.params.slug
-console.log($productSlug)
-</script>
+<script lang="ts">
+  import { sanityClient } from "$lib/sanityClient.js";
+   import imageUrlBuilder from "@sanity/image-url" 
+    function forImgurl(image:any){
+      let builder=imageUrlBuilder(sanityClient);
+      let imageUrl = builder.image(image).url();
+      return imageUrl;
+  }
 
+ export let data;
+  
+ let productimageurl = forImgurl(data.productData.image[0]);
+ console.log(productimageurl)
+</script>
+<h1>{data.productData.name}</h1>
+<img src={productimageurl} alt="">
